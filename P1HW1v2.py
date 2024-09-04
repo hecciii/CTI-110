4d98b9e6692591a9c24a53fe1017d44059493d8a
Samari Franklin <97545406+hecciii@users.noreply.github.com>
2024-09-04 18:36:18 +0000

    new python thing

---
+++ P1HW1v2.py
@@ -0,0 +1,29 @@
+
+# Samari Franklin
+# 9/4/2024
+# P1HW1
+# Writing code that collects information from user, Processes information collected and displays results to users with integers.
+
+print("-----Calculating Exponents-----")
+
+print() 
+
+print()
+base_value = int(input("Enter an integer as the base value: "))
+
+exponent = int(input("Enter an integer as the exponent: "))
+
+print(base_value, "raised to the power of", exponent, "is", base_value ** exponent, "!!")
+
+print("-----Addition and Subtraction-----")
+ 
+print()
+starting_number = int(input("Enter a starting integer: "))
+
+add_number = int(input("Enter an integer to add: "))
+
+sub_number = int(input("Enter an integer to subtract: "))
+
+print()
+
+print(starting_number,"+",add_number,"-",sub_number,"is equal to",starting_number + add_number - sub_number)
