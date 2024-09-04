29e7d22670884f1e8cfbacd339a7fdf415dbb181
Samari Franklin <97545406+hecciii@users.noreply.github.com>
2024-09-04 17:58:30 +0000

    Changes made on file

---
+++ P1LAB1.py
@@ -0,0 +1,13 @@
+# Samari Franklin
+# 9/4/2024
+# P1LAB1
+# This is an example on user input and display output
+
+#The input function always saves data as String by default
+first_name = input("Enter your first name: ")
+last_name = input("Enter is your last name: ")
+print()
+print("Hello", first_name, last_name +"!", "Welcome to CTI-110")
+print()
+
+print(type(first_name))
